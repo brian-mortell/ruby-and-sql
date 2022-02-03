7302d006beec0eea7ecd39f5079ce2aabfff500f
brian-mortell <brian.mortell@kellogg.northwestern.edu>
2022-02-03 02:25:42 +0000

    work through lab 1

--- code-along/1-models.rb
+++ code-along/1-models.rb
@@ -11,10 +11,64 @@ Company.destroy_all
 # 1a. check out the schema file
 # 1b. check out the model file
 
+puts "There are #{Company.all.count} companies"
+
 # 2. create new companies
+
+values = {
+    name: "Apple",
+    url: "https://apple.com",
+    city: "Cupertino",
+    state: "CA"
+}
+company = Company.new(values)
+company.save
+
+values = {
+    name: "Amazon",
+    url: "https://amazon.com",
+    city: "Seattle",
+    state: "WA"
+}
+company = Company.new(values)
+company.save
+
+
+company = Company.new
+company.write_attribute(:name, "Tesla")
+#can also just do company.name = "Tesla"
+company.write_attribute(:url, "https://tesla.com")
+company.write_attribute(:city, "Palo Alto")
+company.write_attribute(:state, "CA")
+company.save
+
+puts "There are #{Company.all.count} companies"
+
 
 # 3. query companies table
 
+# puts Company.all.inspect
+
+# companies = Company.where({state:"CA"})
+# puts companies.inspect
+
+puts Company.where({state:"CA", name: "Apple"}).count
+apple = Company.where({state:"CA", name: "Apple"})[0]
+puts apple.inspect
+
+
 # 4. read column values from row
+# puts "#{apple.read_attribute(:name)} has a website: #{apple.read_attribute(:url)}"
+puts "#{apple.name} has a website #{apple.url}"
 
 # 5. update attribute value
+# apple.write_attribute(:slogan, "Think Different.")
+apple.slogan = "Think Different."
+apple.save
+
+puts apple.inspect
+
+tesler = Company.new({name: "Tesler"})
+tesler.save
+
+tesler.destroy
--- labs/1-models.rb
+++ labs/1-models.rb
@@ -10,10 +10,56 @@ Contact.destroy_all
 
 # 1a. check out the schema file
 # 1b. check out the model file
+apple = Company.where({name: "Apple"})[0]
+puts apple.id
+amazon = Company.where({name: "Amazon"})[0]
+puts amazon.id
+tesla = Company.where({name: "Tesla"})[0]
+puts tesla.id
 
 # 2. create 1-2 new contacts for each company (they can be made up)
 
+contact = Contact.new
+contact.first_name = "Andy"
+contact.last_name = "Jassy"
+contact.email = "andy@amazon.com"
+contact.company_id = amazon.id
+contact.save
+
+contact = Contact.new
+contact.first_name = "Craig"
+contact.last_name = "Federighi"
+contact.email = "craig@apple.com"
+contact.company_id = apple.id
+contact.save
+
+contact = Contact.new
+contact.first_name = "Elon"
+contact.last_name = "Musk"
+contact.email = "elon@tesla.com"
+contact.company_id = tesla.id
+contact.save
+
+contact = Contact.new
+contact.first_name = "Tim"
+contact.last_name = "Cook"
+contact.email = "tim@apple.com"
+contact.company_id = apple.id
+contact.save
+
 # 3. write code to display how many contacts are in the database AND each contact's info (name, email), e.g.:
+andy = Contact.where({first_name: "Andy"})[0]
+craig = Contact.where({first_name: "Craig"})[0]
+elon = Contact.where({first_name: "Elon"})[0]
+tim = Contact.where({first_name: "Tim"})[0]
+
+puts "Contacts: #{Contact.all.count}"
+puts "#{andy.first_name} - #{andy.email}"
+puts "#{craig.first_name} - #{craig.email}"
+puts "#{elon.first_name} - #{elon.email}"
+puts "#{tim.first_name} - #{tim.email}"
+
+#inefficient way to do it
 
 # ---------------------------------
 # Contacts: 4
@@ -21,3 +67,11 @@ Contact.destroy_all
 # Craig Federighi - craig@apple.com
 # Elon Musk - elon@tesla.com
 # Tim Cook - tim@apple.com
+
+puts "Contacts: #{Contact.all.count}"
+contacts = Contact.all
+for contact in contacts
+        puts "#{contact.first_name} #{contact.last_name} - #{contact.email}"
+end
+
+#efficient way to do it - loop!
